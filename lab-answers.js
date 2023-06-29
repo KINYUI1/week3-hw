02d885a218617da6763ffb0f9560e299c6ee8f42
clement <ndiclementkinyui@gmail.com>
2023-06-29 16:49:13 -0500

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -4,12 +4,26 @@ for(let i = 1; i <= 20; i++){
     console.log(i);
 }
 
-//#######################################
+//even numbers in 0 through 200
 
 console.log("even numbers from 0 - 200");
 
 for(let i = 0;i <= 200; i++){
     if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
+//Fizz Buzz
+
+for(let i =1;i <= 100; i++){
+    if(i % 3 === 0  && i % 5 === 0){
+        console.log('FizzBuzz');
+    }else if(i % 3 === 0){
+        console.log('Fizz');
+    }else if(i % 5 === 0){
+        console.log('Buzz');
+    }else{
         console.log(i);
     }
 }
