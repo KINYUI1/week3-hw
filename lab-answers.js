80175b46071c4feb976c30a18d81d034ff6e0bd0
clement <ndiclementkinyui@gmail.com>
2023-06-29 16:35:51 -0500

    Get Even answered

---
+++ lab-answers.js
@@ -0,0 +1,15 @@
+// for loop that that log 1 - 20
+
+for(let i = 1; i <= 20; i++){
+    console.log(i);
+}
+
+//#######################################
+
+console.log("even numbers from 0 - 200");
+
+for(let i = 0;i <= 200; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
